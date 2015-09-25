6c7853c51cea4a14c5156be5eb533845280c4e05
Marcelo Leandro Tavares Medeiros <marceloltm@hotmail.com>
2015-09-25 15:37:15 -0300

    Second Change

--- import.py
+++ import.py
@@ -28,9 +28,13 @@ EXPORT_NAME = 'backup'  # Export Domain Name
 CLUSTER_NAME = 'local_host-Local'  # Cluster Name
 DC_NAME = 'local_host-Local'  # Data Center Name
 STORAGE_NAME = 'local_host-Local'  # Import VM Storage Name
+dc = api.datacenters.get(DC_NAME)
 # Attch Export Domain
 try:
-    if api.datacenters.get(DC_NAME).storagedomains.add(api.storagedomains.get(EXPORT_NAME)):
+    if dc.storagedomains.get(EXPORT_NAME):
+        print 'Exporta Domain Attach'
+    else:
+        dc.storagedomains.add(api.storagedomains.get(EXPORT_NAME))
         print 'Export Domain was attached successfully'
 
 except Exception as e:
@@ -42,9 +46,13 @@ for vm in api.storagedomains.get(EXPORT_NAME).vms.list():
     if api.vms.get(VM_NAME):
         print 'Deleting previous version of VM'
         api.vms.get(VM_NAME).delete()
+    # Import the VM
     try:
-        api.storagedomains.get(EXPORT_NAME).vms.get(VM_NAME).import_vm(params.Action(storage_domain=api.storagedomains.get(STORAGE_NAME), cluster=api.clusters.get(name=CLUSTER_NAME)))
-        # Import the VM
+        sd = api.storagedomains.get(EXPORT_NAME)
+        vm_i = sd.vms.get(VM_NAME)
+        sd_i = api.storagedomains.get(STORAGE_NAME)
+        cl = api.clusters.get(name=CLUSTER_NAME)
+        vm_i.import_vm(params.Action(storage_domain=sd_i, cluster=cl))
         print 'Import the VM started'
         while api.vms.get(VM_NAME).status.state != 'down':
             time.sleep(1)
@@ -53,14 +61,15 @@ for vm in api.storagedomains.get(EXPORT_NAME).vms.list():
     print 'VM Imported Sucessfully'
     # Excluding VM from Export Domain
     print 'Excluding VM from Export Domain'
-    SD = api.storagedomains.get(EXPORT_NAME)
-    VMB = SD.vms.get(VM_NAME)
-    VMB.delete()
-    while VM_NAME in [vm.name for vm in api.storagedomains.get(EXPORT_NAME).vms.list()]:
-        time.sleep(1)
+    sd = api.storagedomains.get(EXPORT_NAME)
+    vmb = sd.vms.get(VM_NAME)
+    vmb.delete()
+    while sd.vms.get(VM_NAME):
+        time.sleep(5)
+time.sleep(5)
 print 'Set Status Maintanence Export Domain'
-api.datacenters.get(DC_NAME).storagedomains.get(EXPORT_NAME).deactivate()
-while api.datacenters.get(DC_NAME).storagedomains.get(EXPORT_NAME).status.state != 'maintenance':
+dc.storagedomains.get(EXPORT_NAME).deactivate()
+while dc.storagedomains.get(EXPORT_NAME).status.state != 'maintenance':
     time.sleep(1)
 print 'Detached Export Storage Domain'
 api.datacenters.get(DC_NAME).storagedomains.get(EXPORT_NAME).delete()
