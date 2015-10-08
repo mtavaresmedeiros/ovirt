c8fe5e1ac23cff13142d9f1aea6f787f0a023b93
Marcelo Leandro Tavares Medeiros <marceloltm@hotmail.com>
2015-10-08 16:29:34 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+TIME_LIMIT = 400
+SLEEP = 10
+TIME_LIMIT_EX = 1800
+SS_N = 'BKP the VM'  # Description the VM snapshot
+EXPORT_NAME = 'backup'  # Export Name
+DC_NAME = 'local_host-Local'  # Domain Name
+CLUSTER_NAME = 'local_host-Local'  # Cluster Name
+EXPORT_NAME = 'backup'  # Export Name
